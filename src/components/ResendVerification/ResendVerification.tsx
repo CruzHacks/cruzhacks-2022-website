3844fea6be4512bb10f08d43318ce8b8c129de3f
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-08-19 14:46:49 -0700

    added recaptcha

--- src/components/ResendVerification/ResendVerification.tsx
+++ src/components/ResendVerification/ResendVerification.tsx
@@ -1,22 +1,54 @@
-import React from "react"
+import React, { useState } from "react"
 import "./ResendVerification.scss"
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios"
+import Recaptcha from "react-recaptcha"
 import { resendVerificationEmail } from "../../utils/Api"
 import { VerificationEmailProps } from "../../Props/props"
+
+const RECAPTCHA_SITE_KEY = process.env.REACT_APP_RECAPTCHA_SITE_KEY || ""
+const RECAPTCHA_VERIFICATION_ENDPOINT =
+  `${process.env.REACT_APP_RECAPTCHA_VERIFICATION_ENDPOINT}/submit` || ""
 
 const ResendVerification: React.FC<VerificationEmailProps> = ({
   user,
   token,
-}: VerificationEmailProps) => (
-  <>
-    <div className='reset-verification'>
-      <button
-        type='button'
-        onClick={() => resendVerificationEmail(user, token)}
-      >
-        Resend Verification Email
-      </button>
-    </div>
-  </>
-)
+}: VerificationEmailProps) => {
+  const [verified, setVerified] = useState<boolean>(false)
+  const verifyToken = (res: string) => {
+    const axiosConfig: AxiosRequestConfig = {
+      headers: {
+        token: res,
+      },
+    }
+    axios
+      .post(RECAPTCHA_VERIFICATION_ENDPOINT, {}, axiosConfig)
+      .then((response: AxiosResponse) => {
+        if (response.status === 200) {
+          return setVerified(true)
+        }
+        return response
+      }) // after response 200, token validated: unlock verification button
+      .catch(err => err)
+  }
+  return (
+    <>
+      <div className='reset-verification'>
+        <Recaptcha
+          sitekey={RECAPTCHA_SITE_KEY}
+          render='explicit'
+          verifyCallback={verifyToken}
+          onloadCallback={() => setVerified(false)}
+        />
+        <p>{`verified? ${verified}`}</p>
+        <button
+          type='button'
+          onClick={() => resendVerificationEmail(user, token)}
+        >
+          Resend Verification Email
+        </button>
+      </div>
+    </>
+  )
+}
 
 export default ResendVerification
