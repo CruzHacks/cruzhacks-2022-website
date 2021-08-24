db4453a7e996f808f4f6ddb5281e0238578dbbba
Krishna Kumar Pandian <krishnakpandian@gmail.com>
2021-08-23 18:17:01 -0700

    Adding fonts and CSS infrastructure (#26)
    * Adding fonts and CSS infrastructure
    
    * Adding EOF
    
    * Removing Debug Line

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.scss"
 import { BrowserRouter as Router } from "react-router-dom"
 import Auth0ProviderWithHistory from "./auth/Auth0ProviderWithHistory"
 import App from "./App"
