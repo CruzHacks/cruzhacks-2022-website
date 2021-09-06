fb01df96cee962c2770dfebe8a65d6e0cd366cec
Krishna Pandian <krishnakpandian@gmail.com>
2021-09-05 23:54:32 -0700

    Removing Resend from the Portal View

--- src/views/Portal/index.view.tsx
+++ src/views/Portal/index.view.tsx
@@ -1,30 +1,8 @@
-import React, { useState, useEffect } from "react"
-import { useAuth0 } from "@auth0/auth0-react"
-import ResendVerification from "../../components/ResendVerification"
+import React from "react"
 import "./index.scss"
 
-const PortalView: React.FC = () => {
-  const { user, getAccessTokenSilently } = useAuth0()
-  const [token, setToken] = useState<string>("")
-  const [, setError] = useState<string>("")
-  useEffect(() => {
-    if (user?.email_verified) {
-      getAccessTokenSilently()
-        .then(accessToken => {
-          setError("")
-          setToken(accessToken)
-        })
-        .catch(err => {
-          setError(err)
-          setToken("")
-        })
-    }
-  }, [])
-  return (
-    <div className='portal-view'>
-      <ResendVerification user={user ? user?.sub : ""} token={token} />
-    </div>
-  )
-}
+const PortalView: React.FC = () => (
+  <div className='portal-view'>Portal Renders</div>
+)
 
 export default PortalView
