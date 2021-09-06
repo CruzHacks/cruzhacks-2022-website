78bbc1595f6d144d3cac36d49cb613fa967bfe55
Sidhanta Sharma <ssharm73@myucsc.edu>
2021-09-06 00:26:52 -0700

    made user field more complete

--- __mocks__/@auth0/auth0-react.js
+++ __mocks__/@auth0/auth0-react.js
@@ -1,5 +1,13 @@
 const useAuth0 = () => ({
-  user: "random user",
+  // to change the values of email_verified and "https://curzhacks.com/roles"
+  // you need to call the mockReturnValue method inside of the "it" block when testing
+  // Ex: "email_verified.mockReturnValueOnce("false")
+  user: {
+    email_verified: jest.fn().mockReturnValue("true"),
+    email: "mock email",
+    "https://cruzhacks.com/roles": jest.fn().mockReturnValue(["User"]),
+    sub: "mock user",
+  },
   getAccessTokenSilently: jest
     .fn()
     .mockImplementation(() => Promise.resolve("Mocked Access Token")),
