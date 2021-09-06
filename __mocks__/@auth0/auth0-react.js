02ec251619f6376e97469d82ce1a3e2ee51a368c
Sidhanta Sharma <ssharm73@myucsc.edu>
2021-09-05 22:21:07 -0700

    refactored code to be a maual mock

--- __mocks__/@auth0/auth0-react.js
+++ __mocks__/@auth0/auth0-react.js
@@ -1,10 +1,8 @@
-const { useAuth0 } = require("@auth0/auth0-react")
-
-jest.mock("@auth0/auth0-react", () => ({ useAuth0: jest.fn() }))
-
-useAuth0.mockReturnValue({
+const useAuth0 = () => ({
   user: "random user",
   getAccessTokenSilently: jest
     .fn()
     .mockImplementation(() => Promise.resolve("Mocked Access Token")),
 })
+
+export default { useAuth0 }
