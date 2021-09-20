712df162431732e69803ecf3edc75a1be5ea44a1
Krishna Kumar Pandian <krishnakpandian@gmail.com>
2021-09-20 16:22:27 -0700

    Chore/remaining macros (#35)
    * First config commit for UI sprint
    
    * Remaining fonts
    
    * Reformatting fonts
    
    Co-authored-by: Alperdec <49699829+Alperdec@users.noreply.github.com>

--- src/views/Home/index.view.tsx
+++ src/views/Home/index.view.tsx
@@ -1,20 +1,24 @@
 import * as React from "react"
 import "./index.scss"
 import About from "./components/About"
-import FAQ from "./components/FAQ"
-import Sponsors from "./components/Footer/Sponsors"
+// import FAQ from "./components/FAQ"
+// import Sponsors from "./components/Sponsors"
+// import Tracks from "./components/Tracks"
 import Landing from "./components/Landing"
 import Milestones from "./components/Milestones"
-import Tracks from "./components/Tracks"
+import NavBar from "../../components/NavBar/NavBar"
+import Footer from "../../components/Footer/Footer"
 
 const HomeView: React.FC = () => (
   <div className='home-view'>
+    <NavBar />
     <Landing />
     <About />
     <Milestones />
-    <Tracks />
+    {/* <Tracks />
     <FAQ />
-    <Sponsors />
+    <Sponsors /> */}
+    <Footer />
   </div>
 )
 
