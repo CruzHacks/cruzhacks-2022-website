48e54ad4a5bd4b416072093b327339fdea9d574b
Krishna Pandian <krishnakpandian@gmail.com>
2021-09-22 23:02:47 -0700

    Modifying Error View

--- src/views/Error/index.view.tsx
+++ src/views/Error/index.view.tsx
@@ -1,10 +1,25 @@
 import * as React from "react"
 import "./index.scss"
+import { useHistory } from "react-router-dom"
+import Button from "../../components/Button/Button"
 
-const ErrorView: React.FC = () => (
-  <div className='error-view'>
-    <p>Error Page Renders</p>
-  </div>
-)
+const ErrorView: React.FC = () => {
+  const history = useHistory()
+
+  const redirect = () => {
+    history.push("/")
+  }
+  return (
+    <div className='error-view'>
+      <div className='error-view-text'>
+        <div className='error-view-message'>error</div>
+        <div className='error-view-message'>404</div>
+      </div>
+      <Button label='error-view-button' onClick={redirect}>
+        Return Home
+      </Button>
+    </div>
+  )
+}
 
 export default ErrorView
