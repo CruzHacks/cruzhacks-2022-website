8c222ef669e77bddd03a698f7c5f7d699a6d0dcf
Krishna Pandian <krishnakpandian@gmail.com>
2021-09-22 22:17:18 -0700

    Desktop View implementation

--- src/Props/props.ts
+++ src/Props/props.ts
@@ -2,3 +2,10 @@ export interface VerificationEmailProps {
   user: string | undefined
   token: string
 }
+
+export interface MilestoneProps {
+  image: string,
+  milestoneName: string,
+  milestoneStat: string,
+  id: string
+}
--- src/views/Home/components/Milestones/index.tsx
+++ src/views/Home/components/Milestones/index.tsx
@@ -1,9 +1,30 @@
 import * as React from "react"
 import "./index.scss"
+import { MilestoneProps } from "../../../../Props/props"
+import milestoneResults from "../../../../Props/milestones/milestones"
 
 const Milestones: React.FC = () => (
   <div className='milestones-component'>
-    <p>Milestones</p>
+    <div className='milestones-component-header'>
+      <div className='milestones-component-header-text'>Milestones of 2021</div>
+      <div className='milestones-component-header-ellipse' />
+    </div>
+
+    <div className='milestones-component-achievements'>
+      {milestoneResults.map(
+        ({ image, milestoneName, milestoneStat, id }: MilestoneProps) => (
+          <div className='milestone' key={id}>
+            <div className='milestone image'>
+              <img src={image} alt='error' />
+            </div>
+            <div className='milestone box'>
+              <div className='milestone stat'> {milestoneStat}</div>
+              <div className='milestone name'> {milestoneName}</div>
+            </div>
+          </div>
+        )
+      )}
+    </div>
   </div>
 )
 export default Milestones
