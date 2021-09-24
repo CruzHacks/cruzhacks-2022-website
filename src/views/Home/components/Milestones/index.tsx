b29799fb0f9a98de3e892fc07e7afd3cc6824908
Krishna Pandian <krishnakpandian@gmail.com>
2021-09-24 16:06:37 -0700

    First draft of milestones

--- src/views/Home/components/Milestones/index.tsx
+++ src/views/Home/components/Milestones/index.tsx
@@ -2,12 +2,23 @@ import * as React from "react"
 import "./index.scss"
 import { MilestoneProps } from "../../../../Props/props"
 import milestoneResults from "../../../../Props/milestones/milestones"
+import milestonesBorderMobile from "../../../../assets/milestones/milestone_header_mobile.svg"
 
 const Milestones: React.FC = () => (
   <div className='milestones-component'>
     <div className='milestones-component-header'>
-      <div className='milestones-component-header-text'>Milestones of 2021</div>
-      <div className='milestones-component-header-ellipse' />
+      <div className='milestones-component-header-circle'>
+        <div className='milestones-component-header-text desktop'>
+          Milestones of 2021
+        </div>
+        <div className='milestones-component-header-text mobile'>
+          Milestones of 2021
+        </div>
+      </div>
+      <div className='milestones-component-header-border'>
+        <img src={milestonesBorderMobile} alt='error' />
+      </div>
+      <div className='milestones-component-header-ellipse mobile' />
     </div>
 
     <div className='milestones-component-achievements'>
