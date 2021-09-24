fc8e14349a6aed48548d549f8063c298fbbc60b2
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-09-23 17:03:55 -0700

    fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,30 @@
 import * as React from "react"
 import "./Footer.scss"
 
-const Footer: React.FC = () => (
-  <div className='footer-component'>Footer Renders</div>
-)
+interface social {
+  link: string
+  icon: string
+  label: string
+}
+interface FooterProps {
+  socials: social[]
+}
 
+const Footer: React.FC<FooterProps> = ({ socials }: FooterProps) => (
+  <div className='footer-component'>
+    <div className='footer-icons'>
+      {socials.map(({ link, icon, label }) => (
+        <a
+          key={label}
+          href={link}
+          target='_blank'
+          rel='noreferrer'
+          className='footer_link-wrapper'
+        >
+          <img className='footer_link-icon' src={icon} alt={label} />
+        </a>
+      ))}
+    </div>
+  </div>
+)
 export default Footer
