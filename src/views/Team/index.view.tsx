c1e55ff8c667ccecb7da438773314614b2211750
Sidhanta Sharma <32908234+sidhantas@users.noreply.github.com>
2021-10-03 23:44:09 -0700

    Feature/team page component (#48)
    * created components and team page
    
    * removed unneccessary css
    
    * added card component
    
    * added our team phrase
    
    * added typography
    
    * created everyones card, no pictures
    
    * fixed requested fixes
    
    * fixed card
    
    * added LinkedInLinks
    
    * fixed key error
    
    * made spacing better
    
    * made spacing better again
    
    Co-authored-by: ssharm73 <ssharm73@ucsc.edu>
    Co-authored-by: Sidhanta Sharma <ssharm73@myucsc.edu>

--- src/views/Team/index.view.tsx
+++ src/views/Team/index.view.tsx
@@ -1,12 +1,21 @@
-import * as React from "react"
+import React from "react"
 import "./index.scss"
 import NavBar from "../../components/NavBar/NavBar"
 import Footer from "../../components/Footer/Footer"
+import TeamGrid from "../../components/TeamCard/TeamCard"
 
 const TeamView: React.FC = () => (
   <div className='team-view'>
     <NavBar />
-    <p>Team Page Renders</p>
+    <div className='team-view__our-team'>OUR TEAM</div>
+    <div className='team-view__small-message'>
+      <div>Meet our team of creators, thinkers,</div>
+      <div>strategists, and believers, collaborating</div>
+      <div>together to create something amazing.</div>
+    </div>
+    <div className='team-view__team'>
+      <TeamGrid />
+    </div>
     <Footer />
   </div>
 )
