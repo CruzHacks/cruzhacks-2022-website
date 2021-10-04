c0f822e957290782f2808875553a1e7402a3b2b9
Krishna Pandian <krishnakpandian@gmail.com>
2021-10-04 10:46:15 -0700

    Fixing mobile with new asset

--- src/views/Home/components/About/index.tsx
+++ src/views/Home/components/About/index.tsx
@@ -1,14 +1,14 @@
 import * as React from "react"
 import "./index.scss"
-import { ReactComponent as BackgroundLineDesktop } from "images/icons/about-background-line.svg"
-// Import ReactComponent Mobile Here Update
+import { ReactComponent as BackgroundLineMobile } from "images/icons/about-background-line-mobile.svg"
+import { ReactComponent as BackgroundLineDesktop } from "images/icons/about-background-line-desktop.svg"
 import { AboutProps } from "Props/props"
 import AboutInformation from "Props/about/about"
 import AboutBox from "./AboutBox/AboutBox"
 
 const About: React.FC = () => (
   <div className='about-component'>
-    <BackgroundLineDesktop className='about-component__background mobile' />
+    <BackgroundLineMobile className='about-component__background mobile' />
     <BackgroundLineDesktop className='about-component__background desktop' />
     <div className='about-component__information'>
       {AboutInformation.map(({ title, description }: AboutProps) => (
