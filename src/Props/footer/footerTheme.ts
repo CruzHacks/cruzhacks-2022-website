ae464c22afcebaaa387256b60574b348b8ec4ede
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-10-04 11:40:05 -0700

    Feature/footer component (#38)
    * initial commit
    
    * fixes
    
    * added styling, theme switching
    
    * tweaking icon glow
    
    * cleaning css
    
    * removing images
    
    * adding images back in
    
    * hover animation
    
    * forgot to add test
    
    * fixes
    
    * Email subscription component (#41)
    
    * Email subscription component
    
    * Fix status codes
    
    * Use "~" for imports in SCSS
    
    * Tweaks
    
    * Remove unused prop interface
    
    * Validate email
    
    * Feature/error (#40)
    
    * Modifying Error View
    
    * Updating error view
    
    * Updating CSS slightly
    
    * Adding white border to button, changing to BEM format
    
    * Updating fallback font
    
    Co-authored-by: Alperdec <49699829+Alperdec@users.noreply.github.com>
    
    * Feature/team page component (#48)
    
    * created components and team page
    
    * removed unneccessary css
    
    * added card component
    
    * added our team phrase
    
    * added typography
    
    * created everyones card, no pictures
    
    * fixed requested fixes
    
    * fixed card
    
    * added LinkedInLinks
    
    * fixed key error
    
    * made spacing better
    
    * made spacing better again
    
    Co-authored-by: ssharm73 <ssharm73@ucsc.edu>
    Co-authored-by: Sidhanta Sharma <ssharm73@myucsc.edu>
    
    * added props (#52)
    
    Co-authored-by: Sidhanta Sharma <ssharm73@myucsc.edu>
    
    Co-authored-by: Krishna Kumar Pandian <krishnakpandian@gmail.com>
    Co-authored-by: William Gao <wlgao@ucsc.edu>
    Co-authored-by: Sidhanta Sharma <32908234+sidhantas@users.noreply.github.com>
    Co-authored-by: ssharm73 <ssharm73@ucsc.edu>
    Co-authored-by: Sidhanta Sharma <ssharm73@myucsc.edu>

---
+++ src/Props/footer/footerTheme.ts
@@ -0,0 +1,37 @@
+import { theme } from "./props"
+
+// This serves as theming props for the footer component
+// homeFooter will probably be removed later due to the design omission of sponsors
+// homeFooterSponsors will be the primary theme to roll out on production for the /home route
+
+interface FooterTheme {
+  teamFooter: theme
+  homeFooter: theme
+  homeFooterSponsors: theme
+}
+
+const footerThemes: FooterTheme = {
+  teamFooter: {
+    bgColor: "#34486B",
+    iconFill: "#C7DAFF",
+    iconStroke: "#34486B",
+    textColor: "#FDFDEE",
+    hillColor: "#445D88",
+  },
+  homeFooter: {
+    bgColor: "linear-gradient(#3792C6, #2775A1)",
+    iconFill: "#FFEAEA",
+    iconStroke: "#3289BA",
+    textColor: "#FFFFFF",
+    hillColor: "#70A8C8",
+  },
+  homeFooterSponsors: {
+    bgColor: "#020D51",
+    iconFill: "#FFE2E2",
+    iconStroke: "#020D51",
+    textColor: "#FFFFFF",
+    hillColor: "#020D51",
+  },
+}
+
+export default footerThemes
---
+++ src/Props/footer/props.ts
@@ -0,0 +1,17 @@
+export interface social {
+    link: string
+    Icon: any
+    label: string
+  }
+
+export interface theme {
+  bgColor: string
+  iconFill: string
+  iconStroke: string
+  textColor: string
+  hillColor: string
+
+}
+export interface FooterProps {
+    theme : theme
+  }
