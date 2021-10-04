1b17263b6e1a3f4e9cb141ef2eaaee3062cde20a
rvaidun <{ID}+{username}@users.noreply.github.com>
2021-10-03 21:38:18 -0700

    started on about

--- src/views/Home/components/About/index.tsx
+++ src/views/Home/components/About/index.tsx
@@ -2,8 +2,21 @@ import * as React from "react"
 import "./index.scss"
 
 const About: React.FC = () => (
-  <div className='about-component'>
-    <p>About</p>
+  <div className='about-component_wrapper'>
+    <div className='about-component header'>About Cruzhacks</div>
+    <div className='about-component text'>
+      CruzHacks is a hackathon that rewards new ideas and innovation— hackers
+      have the chance to use creative and problem-solving skills to address
+      real-world challenges. Open to individuals with any level of experience,
+      Cruzhacks provides the opportunity to apply knowledge and use
+      industry-leading tools to make an impact.
+    </div>
+    <div className='about-component header'> The Cruzhacks Mission </div>
+    <div className='about-component text'>
+      CruzHacks is a global event where people can collaborate, network, and
+      wield technology to solve problems! Our initiative is to expand diversity
+      in tech by sparking the drive to innovate within all our participants.
+    </div>
   </div>
 )
 
