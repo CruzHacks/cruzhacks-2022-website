aa8a47a619fec325283588f1597dae20c8c1e942
William Gao <wlgao@ucsc.edu>
2021-10-07 11:46:14 -0700

    Restructure milestones mobile UI

--- src/views/Home/components/Milestones/index.tsx
+++ src/views/Home/components/Milestones/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import "./index.scss"
 import { MilestoneProps } from "Props/props"
 import milestoneResults from "Props/milestones/milestones"
+import { ReactComponent as BorderMobile } from "assets/milestones/milestone_header_mobile.svg"
 
 const Milestones: React.FC = () => (
   <div className='milestones-component'>
@@ -11,6 +12,7 @@ const Milestones: React.FC = () => (
           <div className='milestones-component__header-text'>
             Milestones of 2021
           </div>
+          <BorderMobile className='milestones-component__header-border' />
         </div>
         <div className='milestones-component__header-ellipse' />
       </div>
@@ -18,13 +20,17 @@ const Milestones: React.FC = () => (
       <div className='milestones-component__achievements'>
         {milestoneResults.map(
           ({ image, milestoneName, milestoneStat, id }: MilestoneProps) => (
-            <div className='milestone' key={id}>
-              <div className='milestone__image'>
+            <div className='milestones-component__milestone' key={id}>
+              <div className='milestones-component__milestone__image'>
                 <img src={image} alt='error' />
               </div>
-              <div className='milestone__box'>
-                <div className='milestone__stat'> {milestoneStat}</div>
-                <div className='milestone__name'> {milestoneName}</div>
+              <div className='milestones-component__milestone__box'>
+                <div className='milestones-component__milestone__stat'>
+                  {milestoneStat}
+                </div>
+                <div className='milestones-component__milestone__name'>
+                  {milestoneName}
+                </div>
               </div>
             </div>
           )
