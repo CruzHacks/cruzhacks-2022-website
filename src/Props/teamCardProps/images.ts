07a9946eecc963e95f9c6b12ac5ff44b68b12e76
Sidhanta Sharma <32908234+sidhantas@users.noreply.github.com>
2021-10-11 16:28:58 -0700

    fixed gaps for phone (#68)
    * fixed gaps for phone
    
    * fixed naming on cards
    
    * fixed Katies card
    
    * should have fixed team message
    
    * cards a little wider on smaller screens now
    
    * added team photos
    
    * forgot the photos
    
    * Fixing responsiveness for mobile and tablet
    
    * Fixing Navya's name
    
    Co-authored-by: ssharm73 <ssharm73@ucsc.edu>
    Co-authored-by: Krishna Pandian <krishnakpandian@gmail.com>

---
+++ src/Props/teamCardProps/images.ts
@@ -0,0 +1,47 @@
+import sMalik from "images/team/Malik_Sonali.png"
+import aBhatia from "images/team/Bhatia_Ajay.png"
+import cGowdru from "images/team/Gowdru_Chinmay.png"
+import tMa from "images/team/Ma_Tony.png"
+import nRao from "images/team/Rao_Navya.png"
+import rVaidun from "images/team/Vaidun_Rahul.png"
+import aDecarion from "images/team/Decarion_Alper.png"
+import vGuan from "images/team/Guan_Vince.png"
+import tRocha from "images/team/Rocha_Tracy.png"
+import eWinchell from "images/team/Winchell_Eliot.png"
+import eDuong from "images/team/Duong_Eric.png"
+import aKarkare from "images/team/Karkare_Avani.png"
+import kPandian from "images/team/Pandian_Krishna.png"
+import kShah from "images/team/Shah_Khushi.png"
+import hWong from "images/team/Wong_Hannah.png"
+import wGao from "images/team/Gao_William.png"
+import jLi from "images/team/Li_Jiewen.png"
+import gRamos from "images/team/Ramos_Glomarie.png"
+import mSheu from "images/team/Sheu_Michelle.png"
+import kAllen from "images/team/Allen_Katie.png"
+import sSharma from "images/team/Sharma_Sidhanta.png"
+
+const images = {
+  sMalik,
+  aBhatia,
+  cGowdru,
+  kAllen,
+  tMa,
+  nRao,
+  rVaidun,
+  aDecarion,
+  vGuan,
+  tRocha,
+  eWinchell,
+  eDuong,
+  sSharma,
+  aKarkare,
+  kPandian,
+  kShah,
+  hWong,
+  wGao,
+  jLi,
+  gRamos,
+  mSheu,
+}
+
+export default images
