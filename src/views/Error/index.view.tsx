80c8cb729295771ba7d414a5c4b4408b4d7307bc
rvaidun <{ID}+{username}@users.noreply.github.com>
2021-10-10 19:32:47 -0700

    added waves and changed background

--- src/views/Error/index.view.tsx
+++ src/views/Error/index.view.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 import "./index.scss"
 import { useHistory } from "react-router-dom"
+import Waves from "images/error/waves.svg"
+import BiggerWaves from "images/error/waves_dropshadow.svg"
+
 import Button from "../../components/Button/Button"
 
 const ErrorView: React.FC = () => {
@@ -11,7 +14,15 @@ const ErrorView: React.FC = () => {
   }
   return (
     <div className='error-view'>
-      <div>
+      <div className='error-view__wavesdiv'>
+        <img src={Waves} alt='waves' className='error-view__waves' />
+        <img
+          src={BiggerWaves}
+          alt='wavesbig'
+          className='error-view__wavesbig'
+        />
+      </div>
+      <div className='error-view__messagediv'>
         <div className='error-view__message'>E R R O R</div>
         <div className='error-view__message'>404</div>
       </div>
