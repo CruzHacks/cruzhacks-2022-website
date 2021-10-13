b6b8781b98a5ef46e62722d50e4ef8a79e753a05
Sidhanta Sharma <32908234+sidhantas@users.noreply.github.com>
2021-10-12 20:17:22 -0700

    Bug/picture load (#78)
    * fixed load time and added shantelle and Brendan
    
    * removed old pics
    
    Co-authored-by: Sidhanta Sharma <ssharm73@myucsc.edu>

--- src/Props/teamCardProps/images.ts
+++ src/Props/teamCardProps/images.ts
@@ -1,24 +1,26 @@
-import sMalik from "images/team/Malik_Sonali.png"
-import aBhatia from "images/team/Bhatia_Ajay.png"
-import cGowdru from "images/team/Gowdru_Chinmay.png"
-import tMa from "images/team/Ma_Tony.png"
-import nRao from "images/team/Rao_Navya.png"
-import rVaidun from "images/team/Vaidun_Rahul.png"
-import aDecarion from "images/team/Decarion_Alper.png"
-import vGuan from "images/team/Guan_Vince.png"
-import tRocha from "images/team/Rocha_Tracy.png"
-import eWinchell from "images/team/Winchell_Eliot.png"
-import eDuong from "images/team/Duong_Eric.png"
-import aKarkare from "images/team/Karkare_Avani.png"
-import kPandian from "images/team/Pandian_Krishna.png"
-import kShah from "images/team/Shah_Khushi.png"
-import hWong from "images/team/Wong_Hannah.png"
-import wGao from "images/team/Gao_William.png"
-import jLi from "images/team/Li_Jiewen.png"
-import gRamos from "images/team/Ramos_Glomarie.png"
-import mSheu from "images/team/Sheu_Michelle.png"
-import kAllen from "images/team/Allen_Katie.png"
-import sSharma from "images/team/Sharma_Sidhanta.png"
+import sMalik from "images/team/Malik_Sonali.jpg"
+import aBhatia from "images/team/Bhatia_Ajay.jpg"
+import cGowdru from "images/team/Gowdru_Chinmay.jpg"
+import tMa from "images/team/Ma_Tony.jpg"
+import nRao from "images/team/Rao_Navya.jpg"
+import rVaidun from "images/team/Vaidun_Rahul.jpg"
+import aDecarion from "images/team/Decarion_Alper.jpg"
+import vGuan from "images/team/Guan_Vince.jpg"
+import tRocha from "images/team/Rocha_Tracy.jpg"
+import eWinchell from "images/team/Winchell_Eliot.jpg"
+import eDuong from "images/team/Duong_Eric.jpg"
+import aKarkare from "images/team/Karkare_Avani.jpg"
+import kPandian from "images/team/Pandian_Krishna.jpg"
+import kShah from "images/team/Shah_Khushi.jpg"
+import hWong from "images/team/Wong_Hannah.jpg"
+import wGao from "images/team/Gao_William.jpg"
+import jLi from "images/team/Li_Jiewen.jpg"
+import gRamos from "images/team/Ramos_Glomarie.jpg"
+import mSheu from "images/team/Sheu_Michelle.jpg"
+import kAllen from "images/team/Allen_Katie.jpg"
+import sSharma from "images/team/Sharma_Sidhanta.jpg"
+import bTeo from "images/team/Teo_Brendan.jpg"
+import sToh from "images/team/Toh_Shantelle.jpg"
 
 const images = {
   sMalik,
@@ -42,6 +44,8 @@ const images = {
   jLi,
   gRamos,
   mSheu,
+  bTeo,
+  sToh,
 }
 
 export default images
