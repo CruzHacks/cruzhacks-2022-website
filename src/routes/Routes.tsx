5e50c1c1ea1eed15655ab8391f1d41bda5e01889
Krishna Kumar Pandian <krishnakpandian@gmail.com>
2021-10-17 18:20:44 -0700

    Removing Portal View Access for first launc (#75)

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom"
 import { useAuth0 } from "@auth0/auth0-react"
 import PrivateRoute from "../auth/PrivateRoute"
 import HomeView from "../views/Home/index.view"
-import PortalView from "../views/Portal/index.view"
+// import PortalView from "../views/Portal/index.view"
 import TeamView from "../views/Team/index.view"
 import ErrorView from "../views/Error/index.view"
 import DashboardView from "../views/Dashboard/index.view"
@@ -20,7 +20,7 @@ const Routes: React.FC = () => {
       <Switch>
         <Route exact path='/' component={HomeView} />
         <Route exact path='/team' component={TeamView} />
-        <PrivateRoute exact path='/portal' component={PortalView} />
+        {/* <PrivateRoute exact path='/portal' component={PortalView} /> */}
 
         <PrivateRoute
           exact
