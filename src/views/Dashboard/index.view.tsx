93eac57ad948391c40fe688eaba149caea537c70
William Gao <wlgao@ucsc.edu>
2021-11-01 18:00:50 -0700

    Refine admin route (#90)
    * Update
    
    * Refine admin dashboard layout
    
    * Responsiveness
    
    * Changes to announcement form and timer
    
    * Update check for status code
    
    * Update Date format
    
    * Use ternary

--- src/views/Dashboard/index.view.tsx
+++ src/views/Dashboard/index.view.tsx
@@ -1,21 +1,25 @@
 import React from "react"
 import "./index.scss"
 import Navbar from "components/NavBar/NavBar"
-import AddAnnouncement from "./AddAnnouncement/index.view"
-import AnnouncmentsListed from "./AnnouncementsListed/index.view"
-import Analytics from "./Analytics/index.view"
+import AddAnnouncement from "./components/AddAnnouncement/index.view"
+import AnnouncementsListed from "./components/AnnouncementsListed/index.view"
+import Analytics from "./components/Analytics/index.view"
 
 const DashboardView: React.FC = () => (
   <div className='dashboard-view'>
     <Navbar theme='home' />
-    <div className='dashboard-view__header'>Organizer&apos;s View</div>
-    <div className='dashboard-view__components'>
-      <div className='dashboard-view__announcements'>
-        <AddAnnouncement />
-        <AnnouncmentsListed />
-      </div>
-      <div className='dashboard-view__analytics'>
-        <Analytics />
+    <div className='dashboard-view__container'>
+      <div className='dashboard-view__header'>Organizer&apos;s View</div>
+      <div className='dashboard-view__components'>
+        {/* left panel */}
+        <div className='dashboard-view__announcements'>
+          <AddAnnouncement />
+          <AnnouncementsListed />
+        </div>
+        {/* right panel */}
+        <div className='dashboard-view__analytics'>
+          <Analytics />
+        </div>
       </div>
     </div>
   </div>
