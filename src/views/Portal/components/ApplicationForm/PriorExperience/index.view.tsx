8a27159a29eec332a69ae82ec50a3f069cf57510
Krishna Pandian <krishnakpandian@gmail.com>
2021-11-06 13:41:28 -0700

    Fixing slight styling

--- src/views/Portal/components/ApplicationForm/PriorExperience/index.view.tsx
+++ src/views/Portal/components/ApplicationForm/PriorExperience/index.view.tsx
@@ -10,48 +10,50 @@ const ExperiencePage: React.FC = () => {
     useApplication()!
   return (
     <div className='experience-page'>
-      Prior Experience
-      <RadioForm
-        question='Will this be your first time attending CruzHacks?'
-        name='firstCruzHacks'
-        inputs={[{ label: "Yes" }, { label: "No" }]}
-        handleChange={(e: any) =>
-          setPriorExperienceFormData(prev => ({
-            ...prev,
-            firstCruzHacks: e.target.value,
-          }))
-        }
-        errorMessage={priorExperienceFormData.firstCruzHacksErr}
-        value={priorExperienceFormData.firstCruzHacks}
-      />
-      <TextField
-        // className='demographic-page__form-container__inputs--textfield'
-        name='Have you attended any hackathons before? If so, how many?'
-        handleChange={(e: any) =>
-          setPriorExperienceFormData(prev => ({
-            ...prev,
-            hackathonCount: e.target.value,
-          }))
-        }
-        fieldState={priorExperienceFormData.hackathonCount}
-        errorMessage={priorExperienceFormData.hackathonCountErr}
-        label='hackathonCount'
-        maxLength={3}
-      />
-      <TextBox
-        // className='short-answer-page__inputs--textfield'
-        name='Do you have prior tech experience? If so, in what context (i.e., classes, internships, personal projects)?'
-        handleChange={(e: any) =>
-          setPriorExperienceFormData(prev => ({
-            ...prev,
-            priorExperience: e.target.value,
-          }))
-        }
-        fieldState={priorExperienceFormData.priorExperience}
-        errorMessage={priorExperienceFormData.priorExperienceErr}
-        label='priorExperience'
-        maxLength={250}
-      />
+      <div className='experience-page__container'>
+        Prior Experience
+        <RadioForm
+          question='Will this be your first time attending CruzHacks?'
+          name='firstCruzHacks'
+          inputs={[{ label: "Yes" }, { label: "No" }]}
+          handleChange={(e: any) =>
+            setPriorExperienceFormData(prev => ({
+              ...prev,
+              firstCruzHacks: e.target.value,
+            }))
+          }
+          errorMessage={priorExperienceFormData.firstCruzHacksErr}
+          value={priorExperienceFormData.firstCruzHacks}
+        />
+        <TextField
+          // className='demographic-page__form-container__inputs--textfield'
+          name='Have you attended any hackathons before? If so, how many?'
+          handleChange={(e: any) =>
+            setPriorExperienceFormData(prev => ({
+              ...prev,
+              hackathonCount: e.target.value,
+            }))
+          }
+          fieldState={priorExperienceFormData.hackathonCount}
+          errorMessage={priorExperienceFormData.hackathonCountErr}
+          label='hackathonCount'
+          maxLength={3}
+        />
+        <TextBox
+          // className='short-answer-page__inputs--textfield'
+          name='Do you have prior tech experience? If so, in what context (i.e., classes, internships, personal projects)?'
+          handleChange={(e: any) =>
+            setPriorExperienceFormData(prev => ({
+              ...prev,
+              priorExperience: e.target.value,
+            }))
+          }
+          fieldState={priorExperienceFormData.priorExperience}
+          errorMessage={priorExperienceFormData.priorExperienceErr}
+          label='priorExperience'
+          maxLength={250}
+        />
+      </div>
     </div>
   )
 }
