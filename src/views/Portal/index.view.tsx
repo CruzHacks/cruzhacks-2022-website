37e28b64609cb821c9153339e0e4cd71e738ad1d
Sidhanta Sharma <ssharm73@myucsc.edu>
2021-11-05 20:26:44 -0700

    undid view changes

--- src/views/Portal/index.view.tsx
+++ src/views/Portal/index.view.tsx
@@ -1,12 +1,11 @@
 import React from "react"
 import "./index.scss"
-// import { ApplicationProvider } from "components/ApplicationContext/ApplicationContext"
+import { ApplicationProvider } from "components/ApplicationContext/ApplicationContext"
 import ApplicationBackground from "./components/Background/index.view"
-import MainPage from "./components/ApplicationForm/MainPage/index.view"
 
 const PortalView: React.FC = () => (
   <ApplicationBackground>
-    <MainPage status='ACCEPTED' progress={50} />
+    <ApplicationProvider />
   </ApplicationBackground>
 )
 
