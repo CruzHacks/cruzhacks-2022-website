fc22f696762b0b4c5f6851b13b1429d207479a7e
rvaidun <19789246+rvaidun@users.noreply.github.com>
2021-11-07 13:17:41 -0700

    made dropdowns

--- src/views/Home/components/FAQ/index.tsx
+++ src/views/Home/components/FAQ/index.tsx
@@ -6,6 +6,18 @@ import { FAQProps } from "Props/props"
 const col1 = FAQInformation.slice(0, 4)
 const col2 = FAQInformation.slice(4, 8)
 const col3 = FAQInformation.slice(8, 12)
+const showAnswer = (e: React.MouseEvent | React.KeyboardEvent) => {
+  // get next element sibling
+  const target = e.target as HTMLTextAreaElement
+  const next = target.nextElementSibling as HTMLElement
+  if (next) {
+    if (next.style.display === "block") {
+      next.style.display = "none"
+    } else {
+      next.style.display = "block"
+    }
+  }
+}
 const FAQ: React.FC = () => (
   <div className='faq-component'>
     <div className='faq-component__box'>
@@ -14,24 +26,54 @@ const FAQ: React.FC = () => (
       <div className='faq-component__questionsandanswers'>
         <div className='faq-component__column'>
           {col1.map(({ question, answer }: FAQProps) => (
-            <div className='faq-component__questionsandanswersitems'>
-              <div className='faq-component__question'>{question}</div>
+            <div
+              className='faq-component__questionsandanswersitems'
+              key={question}
+            >
+              <div
+                className='faq-component__question'
+                onClick={showAnswer}
+                onKeyDown={showAnswer}
+                role='presentation'
+              >
+                {question}
+              </div>
               <div className='faq-component__answer'>{answer}</div>
             </div>
           ))}
         </div>
         <div className='faq-component__column'>
           {col2.map(({ question, answer }: FAQProps) => (
-            <div className='faq-component__questionsandanswersitems'>
-              <div className='faq-component__question'>{question}</div>
+            <div
+              className='faq-component__questionsandanswersitems'
+              key={question}
+            >
+              <div
+                className='faq-component__question'
+                onClick={showAnswer}
+                onKeyDown={showAnswer}
+                role='presentation'
+              >
+                {question}
+              </div>
               <div className='faq-component__answer'>{answer}</div>
             </div>
           ))}
         </div>
         <div className='faq-component__column'>
           {col3.map(({ question, answer }: FAQProps) => (
-            <div className='faq-component__questionsandanswersitems'>
-              <div className='faq-component__question'>{question}</div>
+            <div
+              className='faq-component__questionsandanswersitems'
+              key={question}
+            >
+              <div
+                className='faq-component__question'
+                onClick={showAnswer}
+                onKeyDown={showAnswer}
+                role='presentation'
+              >
+                {question}
+              </div>
               <div className='faq-component__answer'>{answer}</div>
             </div>
           ))}
