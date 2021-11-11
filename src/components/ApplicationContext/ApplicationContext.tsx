3c040b0ef0523061f51e48f286badde3be618e31
William Gao <wlgao@ucsc.edu>
2021-11-11 10:23:39 -0800

    Switch to sessionStorage

--- src/components/ApplicationContext/ApplicationContext.tsx
+++ src/components/ApplicationContext/ApplicationContext.tsx
@@ -125,15 +125,30 @@ export const ApplicationProvider: React.FC = () => {
               break
             case "accepted":
               setStatus(AppStatus.Accepted)
-              store("applicationStatus", AppStatus.Accepted, 12 * 60 * 60) //  store for 12hrs
+              store(
+                "applicationStatus",
+                AppStatus.Accepted,
+                12 * 60 * 60, // store for 12hrs
+                user?.email
+              )
               break
             case "rejected":
               setStatus(AppStatus.Rejected)
-              store("applicationStatus", AppStatus.Rejected, 12 * 60 * 60) // store for 12hrs
+              store(
+                "applicationStatus",
+                AppStatus.Rejected,
+                12 * 60 * 60, // store for 12hrs
+                user?.email
+              )
               break
             case "pending":
               setStatus(AppStatus.Pending)
-              store("applicationStatus", AppStatus.Pending, 12 * 60 * 60) // store for 12hrs
+              store(
+                "applicationStatus",
+                AppStatus.Pending,
+                12 * 60 * 60, // store for 12hrs
+                user?.email
+              )
               break
             default:
               setStatus(AppStatus.Errored)
