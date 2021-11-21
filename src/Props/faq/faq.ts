4debacaac1364c4907fe27fb9a1fe3b72ce03d93
rvaidun <19789246+rvaidun@users.noreply.github.com>
2021-11-20 21:12:18 -0800

    Feature/faqcomponent (#98)
    * Created FAQ Component
    
    * 3 columns
    
    * made dropdowns
    
    * made faq component responsive
    
    * made faq component line more responsive
    
    * more responsive on mobile devices, width is closer to figma design
    
    * scales better
    
    * added question marks and fixed clipping issues around 800px of width
    
    * added more padding so footer svg doesnt overlap with the faq
    
    * fixed styling for smallest. removed unncessary changes in the footer. fixed missing question problem
    
    * Updating Background
    
    Co-authored-by: Krishna Pandian <krishnakpandian@gmail.com>

---
+++ src/Props/faq/faq.ts
@@ -0,0 +1,69 @@
+import { FAQProps } from "../props"
+
+const FAQInformation: FAQProps[] = [
+  {
+    question: "What is a hackathon?",
+    answer:
+      "UCSC’s hackathon is a three day, weekend-long event where you build your ideas to life, network, and show off your talent. Ideas can be formed through teams or individually. There are also hundreds of students, mentors, sponsors, and judges that can help push your vision forward. Along this event are workshops geared for students of all levels to learn new skills and network with other peers or recruiters.",
+  },
+  {
+    question: "How long is the event?",
+    answer:
+      "CruzHacks is a 3 day weekend event from Friday night to Sunday noon with an options; awards ceremony in the afternoon.",
+  },
+  {
+    question: "Who can participate?",
+    answer:
+      "Anyone and everyone! It is open to anyone who wants to create their vision. No coding or technical experience is necessary. If you are a designer, product manager, artists, business major, etc you are welcome to apply! Mentors will be available to help you through any technical difficulties.",
+  },
+  {
+    question:
+      "Do I need to have a team in place/at all? What would be the limit if so?",
+    answer:
+      "It is not necessary to have a team at the time of application nor at the actual event. Teams must be a max of 4 people.",
+  },
+  {
+    question: "Do I need to know how to code?",
+    answer:
+      "Nope! No experience is required. We’re looking for applicants who are passionate about learning something new and creating a vision. 25% of hackers in 2021 and 47% in 2020 were first time hackers.",
+  },
+  {
+    question: "Do I need to be a Computer Science/Engineering major?",
+    answer: "We welcome and accept people of all majors!",
+  },
+  {
+    question: "Will CruzHacks be virtual or in-person?",
+    answer:
+      "Due to UCSC, Santa Cruz, and California protocols, CruzHacks will remain virtual for CruzHacks 2022. However, there may be a possibility of having a short in-person event for those in the Santa Cruz county that abide by UCSC protocols and feel comfortable to attend.",
+  },
+  {
+    question: "Does it cost anything to participate?",
+    answer: "Cruzhacks is free for all participants!",
+  },
+  {
+    question: "What kind of projects can I make?",
+    answer:
+      "Any project (as technical as you want to get) is acceptable as long as it is part of one of the three main categories: Earth, Health, or Social Justice.",
+  },
+  {
+    question: "Do I have to be a UCSC student?",
+    answer:
+      "Nope! We accept applicants from all over the world. In 2021, 47% of our hackers were non-UCSC students and among that, there were about 100 different schools.",
+  },
+  {
+    question: "Will there be people there to help me with my project?",
+    answer:
+      "Yes! We have at least 25 mentors ready to assist anyone on their project varying all subjects and all skill levels.",
+  },
+  {
+    question: "Will there be any workshops? On what topics?",
+    answer:
+      "We have a wide range of workshops, both technical and non-technical, planned including but not limited to: Intro to Web Design, Basics of HTML, Machine learning basics, and Resume/Cover Letter.",
+  },
+  {
+    question: "What prizes can I expect?",
+    answer:
+      "We are unsure about what the final prizes will be, but in the past we have had Amazon Echo Dots, JBL speakers, light up keyboards, gaming headphones, and much more varying different categories. Sponsors will also give out specific prizes as they see fit. In addition, UCSC students are eligible to receive a t-shirt and stickers if they are in the Santa Cruz County.",
+  },
+]
+export default FAQInformation
