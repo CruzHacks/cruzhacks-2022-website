99f0c9c8e8df0307a3ada49965a629986613bb69
Krishna Kumar Pandian <krishnakpandian@gmail.com>
2021-11-23 22:36:28 -0800

    Updating FAQ content (#130)

--- src/Props/faq/faq.ts
+++ src/Props/faq/faq.ts
@@ -2,39 +2,33 @@ import { FAQProps } from "../props"
 
 const FAQInformation: FAQProps[] = [
   {
-    question: "What is a hackathon?",
+    question: "What is CruzHacks? ",
     answer:
-      "UCSC’s hackathon is a three day, weekend-long event where you build your ideas to life, network, and show off your talent. Ideas can be formed through teams or individually. There are also hundreds of students, mentors, sponsors, and judges that can help push your vision forward. Along this event are workshops geared for students of all levels to learn new skills and network with other peers or recruiters.",
+      "CruzHacks is UCSC’s largest hackathon. It is a three day event where you can build your ideas, network, and show off your talent. Ideas can be formed through teams or individually. There are hundreds of students, mentors, sponsors, and judges that can help push your vision forward. The event also includes workshops geared towards students of all levels to learn and improve their technical skills.",
   },
   {
     question: "How long is the event?",
     answer:
-      "CruzHacks is a 3 day weekend event from Friday night to Sunday noon with an options; awards ceremony in the afternoon.",
+      "CruzHacks is a 3 day event that starts Friday night and ends Sunday afternoon. This year, it will take place January 14-16, 2022.",
   },
   {
     question: "Who can participate?",
     answer:
-      "Anyone and everyone! It is open to anyone who wants to create their vision. No coding or technical experience is necessary. If you are a designer, product manager, artists, business major, etc you are welcome to apply! Mentors will be available to help you through any technical difficulties.",
-  },
-  {
-    question:
-      "Do I need to have a team in place/at all? What would be the limit if so?",
-    answer:
-      "It is not necessary to have a team at the time of application nor at the actual event. Teams must be a max of 4 people.",
+      "All students studying at a high school, college/university, and other alternative schools (such as bootcamps) can apply! CruzHacks is open to designers, project managers, artists, and anyone else interested in creating! Mentors will be available to help you through any technical difficulties. No coding or technical experience is necessary!",
   },
   {
     question: "Do I need to know how to code?",
     answer:
-      "Nope! No experience is required. We’re looking for applicants who are passionate about learning something new and creating a vision. 25% of hackers in 2021 and 47% in 2020 were first time hackers.",
+      "Nope! No experience is required. We’re looking for applicants who are passionate about learning something new and creating a vision. 25% of hackers in 2021 and 47% in 2020 were first time hackers. ",
   },
   {
     question: "Do I need to be a Computer Science/Engineering major?",
-    answer: "We welcome and accept people of all majors!",
+    answer: "Nope! Students of all majors may apply.",
   },
   {
-    question: "Will CruzHacks be virtual or in-person?",
+    question: "Will there be people there to help me with my project?",
     answer:
-      "Due to UCSC, Santa Cruz, and California protocols, CruzHacks will remain virtual for CruzHacks 2022. However, there may be a possibility of having a short in-person event for those in the Santa Cruz county that abide by UCSC protocols and feel comfortable to attend.",
+      "Yes! Mentors with various skills are present throughout the event to assist anyone on their project.",
   },
   {
     question: "Does it cost anything to participate?",
@@ -43,27 +37,28 @@ const FAQInformation: FAQProps[] = [
   {
     question: "What kind of projects can I make?",
     answer:
-      "Any project (as technical as you want to get) is acceptable as long as it is part of one of the three main categories: Earth, Health, or Social Justice.",
+      "Any project, technical or non-technical, is welcome! These projects must fall under a CruzHacks or sponsor category to be considered for prizes.",
   },
   {
     question: "Do I have to be a UCSC student?",
     answer:
-      "Nope! We accept applicants from all over the world. In 2021, 47% of our hackers were non-UCSC students and among that, there were about 100 different schools.",
-  },
-  {
-    question: "Will there be people there to help me with my project?",
-    answer:
-      "Yes! We have at least 25 mentors ready to assist anyone on their project varying all subjects and all skill levels.",
+      "Nope! We accept applicants from all over the world. In 2021, 47% of our hackers were non-UCSC students and hackers attended from 100 different schools around the globe. ",
   },
   {
     question: "Will there be any workshops? On what topics?",
     answer:
-      "We have a wide range of workshops, both technical and non-technical, planned including but not limited to: Intro to Web Design, Basics of HTML, Machine learning basics, and Resume/Cover Letter.",
+      "We have a wide range of workshops, both technical and non-technical, on topics such as Intro to Web Design, Basics of HTML, Machine Learning Basics, and Resume Reviews.",
   },
   {
-    question: "What prizes can I expect?",
+    question: "Will CruzHacks be virtual or in-person?",
     answer:
-      "We are unsure about what the final prizes will be, but in the past we have had Amazon Echo Dots, JBL speakers, light up keyboards, gaming headphones, and much more varying different categories. Sponsors will also give out specific prizes as they see fit. In addition, UCSC students are eligible to receive a t-shirt and stickers if they are in the Santa Cruz County.",
+      "Due to UCSC, Santa Cruz, and California protocols, CruzHacks will remain virtual in 2022. However, we will be distributing swag to all hackers in the Santa Cruz area!",
+  },
+  {
+    question:
+      "Do I need to have a team in place/at all? What would be the limit if so?",
+    answer:
+      "It is not necessary to have a team at the time of application nor at the actual event. If you choose to build a team, there can be a maximum of 4 people.",
   },
 ]
 export default FAQInformation
