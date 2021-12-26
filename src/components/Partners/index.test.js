e0f3eb7d24d12768f49ee6777da8bd8767d7efa3
Krishna Pandian <krishnakpandian@gmail.com>
2021-12-25 19:51:14 -0800

    Adding EOF to unit tests

--- src/components/Partners/index.test.js
+++ src/components/Partners/index.test.js
@@ -1,11 +1,11 @@
-import Enzyme, {render} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Partners from './index.view';
+import Enzyme, { render } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import Partners from "./index.view"
 
 Enzyme.configure({ adapter: new Adapter() })
 
-describe('Testing Partners', () => {
-  it('Partners renders without crashing', () => {
-    render(<Partners />);
-  });
-});
+describe("Testing Partners", () => {
+  it("Partners renders without crashing", () => {
+    render(<Partners />)
+  })
+})
--- src/components/Sponsors/index.test.js
+++ src/components/Sponsors/index.test.js
@@ -1,11 +1,11 @@
-import Enzyme, {render} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Sponsors from './index.view';
+import Enzyme, { render } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import Sponsors from "./index.view"
 
 Enzyme.configure({ adapter: new Adapter() })
 
-describe('Testing Sponsors', () => {
-  it('Sponsors renders without crashing', () => {
-    render(<Sponsors />);
-  });
-});
+describe("Testing Sponsors", () => {
+  it("Sponsors renders without crashing", () => {
+    render(<Sponsors />)
+  })
+})
