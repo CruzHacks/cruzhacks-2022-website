3f97f8ea8e019fd33c6cc7f344b8f2ebb43db6e8
Sidhanta Sharma <32908234+sidhantas@users.noreply.github.com>
2022-01-03 18:51:42 -0800

    Feature/tracks (#155)
    * component works, need to fix SVGS
    
    * fixed SVGS
    
    * fixed SVGS
    
    * completed component
    
    * prettier changes
    
    * removed extra file
    
    * added home view
    
    * moved SVGS to differnet folder, fixed small screen scaling
    
    * fixing tests
    
    * made hover text unselectable
    
    * made golden state bigger
    
    * changed aspect ratio to height
    
    * fixed iphone scaling and organized css
    
    * fixed scaling on smaller screens
    
    * changed gradient
    
    * fixed responsiveness and changed UIUX title
    
    * Fixing margin-top
    
    Co-authored-by: Sidhanta Sharma <ssharm73@myucsc.edu>
    Co-authored-by: Krishna Pandian <krishnakpandian@gmail.com>

---
+++ src/views/Home/components/PrizeTracks/PrizeTracks.tsx
@@ -0,0 +1,87 @@
+import * as React from "react"
+import "./PrizeTracks.scss"
+import { ReactComponent as HealthHacks } from "images/tracks/health_hacks.svg"
+import { ReactComponent as BeginnerHacks } from "images/tracks/beginner_hacks.svg"
+import { ReactComponent as EarthHacks } from "images/tracks/earth_hacks.svg"
+import { ReactComponent as GoldenStateHacks } from "images/tracks/golden_state_hacks.svg"
+import { ReactComponent as JusticeHacks } from "images/tracks/justice_hacks.svg"
+import { ReactComponent as UIUXHacks } from "images/tracks/ui_ux_hacks.svg"
+
+interface CircleProps {
+  image: any
+  hoverText: string
+}
+
+const Tracks = [
+  [
+    <HealthHacks className='prizeTracks--svg-shadow prizeTracks--track-img-dimensions' />,
+    "Health Hacks",
+    0,
+  ],
+  [
+    <JusticeHacks className='prizeTracks--svg-shadow prizeTracks--track-img-dimensions-justice' />,
+    "Justice Hacks",
+    2,
+  ],
+  [
+    <EarthHacks className='prizeTracks--svg-shadow prizeTracks--track-img-dimensions-earth' />,
+    "Earth Hacks",
+    1,
+  ],
+  [
+    <BeginnerHacks className='prizeTracks--svg-shadow prizeTracks--track-img-dimensions' />,
+    "Beginner Hacks",
+    3,
+  ],
+  [
+    <GoldenStateHacks className='prizeTracks--track-img-dimensions-golden' />,
+    "Golden State Hacks",
+    4,
+  ],
+  [
+    <UIUXHacks className='prizeTracks--svg-shadow prizeTracks--track-img-dimensions prizeTracks--UIUX' />,
+    "UI/UX Hacks",
+    5,
+  ],
+]
+
+const Circle: React.FC<CircleProps> = ({ image, hoverText }: CircleProps) => {
+  const [isHovered, setHover] = React.useState<boolean>(false)
+  return (
+    <div
+      className='prizeTracks__images__container'
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {isHovered ? (
+        <div className='prizeTracks__images__container--hover'>
+          <div className='prizeTracks--unselectable'>{hoverText}</div>
+        </div>
+      ) : null}
+      <div className='prizeTracks__images__container--image'>{image}</div>
+    </div>
+  )
+}
+
+const PrizeTracks: React.FC = () => (
+  <div className='prizeTracks'>
+    <div className='prizeTracks__title'>
+      <div className='prizeTracks__title__container'>
+        <div className='prizeTracks__title__container--header'>
+          Prize Tracks
+        </div>
+      </div>
+    </div>
+    <div className='prizeTracks__images'>
+      {Tracks.map(image => (
+        <Circle
+          image={image[0]}
+          hoverText={image[1] as string}
+          key={image[2] as number}
+        />
+      ))}
+    </div>
+  </div>
+)
+
+export default PrizeTracks
