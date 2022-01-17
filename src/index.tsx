3bc75a57283cf6ff5576306a6a22da83515c360f
Krishna Kumar Pandian <krishnakpandian@gmail.com>
2022-01-17 12:31:31 -0800

    Post Event Update for 2022 (#175)
    * Post Event Update for 2022
    
    * Changing title to 2022

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
-import Auth0ProviderWithHistory from "./auth/Auth0ProviderWithHistory"
+// import Auth0ProviderWithHistory from "./auth/Auth0ProviderWithHistory"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 
 // constants
-const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || ""
-const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENTID || ""
-const AUTH0_AUDIENCE = process.env.REACT_APP_AUTH0_AUDIENCE || ""
+// const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || ""
+// const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENTID || ""
+// const AUTH0_AUDIENCE = process.env.REACT_APP_AUTH0_AUDIENCE || ""
 
 ReactDOM.render(
   <Router>
-    <Auth0ProviderWithHistory
+    {/* <Auth0ProviderWithHistory
       domain={AUTH0_DOMAIN}
       clientId={AUTH0_CLIENT_ID}
       audience={AUTH0_AUDIENCE}
       redirectUri={window.location.origin}
-    >
-      <App />
-    </Auth0ProviderWithHistory>
+    > */}
+    <App />
+    {/* </Auth0ProviderWithHistory> */}
   </Router>,
   document.getElementById("root")
 )
